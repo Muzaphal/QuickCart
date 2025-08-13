ca71bd7ed5a54da327f024547f2443402490a2a9
muzaphal <muzaphalisa69@gmail.com>
2025-08-13 16:44:04 +0100

    First Commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,9 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
+import { 
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdate } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     imageUrl: { type: String, require: true },
-    cartItems: { type: object, default: {} },
+    cartItems: { type: Object, default: {} },
 }, {minimize: false })
 
 const User = mongoose.models.user || mongoose.model('user', userSchema)
